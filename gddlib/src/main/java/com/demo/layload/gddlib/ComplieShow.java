d75e9edd98d822011bb4e02b9f0f5efc8115513c
gongdongdong <ahgw33@163.com>
2018-08-31 17:20:12 +0800

    [add]新增加项目

---
+++ gddlib/src/main/java/com/demo/layload/gddlib/ComplieShow.java
@@ -0,0 +1,14 @@
+package com.demo.layload.gddlib;
+
+/**
+ * Created by gongdongdong on 2018/8/31.
+ */
+
+public class ComplieShow {
+
+    public enum BEHAVIOUR {
+
+        SHOW, HIDE, WAIT, DO
+    }
+
+}
